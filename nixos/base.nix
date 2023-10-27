2ece43697fbefc1bf0ce004a8cc5507af121acf6
Colin <cm@mntg.ca>
2023-10-27 16:34:18 -0300

    flakes!

--- nixos/base.nix
+++ nixos/base.nix
@@ -80,7 +80,7 @@
         options = let
         # this line prevents hanging on network split
         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,uid=colin";
-        in ["${automount_opts},credentials=/etc/nixos//secrets/smbsecrets"];
+        in ["${automount_opts},credentials=../secrets/smbsecrets"];
     };
 
     nixpkgs.config.allowUnfree = true;
