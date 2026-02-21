fb2eff8fabc387f3f078e71857e5d33f12431cb2
Colin <cm@mntg.ca>
2026-02-21 18:55:21 -0400

    Remove desktop, server, and vps hosts - keep only Surface Go config

--- flake.nix
+++ flake.nix
@@ -25,40 +25,12 @@
     # NixOS configuration entrypoint
     # Available through 'nixos-rebuild --flake .#your-hostname'
     nixosConfigurations = {
-      desktop = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          ./machines/desktop/desktop.nix
-          ./modules/bluetooth.nix
-          ./machines/base.nix
-          ./modules/network-storage.nix
-          ./modules/sunshine.nix
-        ];
-      };
-
       go = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         modules = [
           ./machines/go/go.nix
           ./modules/bluetooth.nix
           ./machines/base.nix
-          ./modules/network-storage.nix
-        ];
-      };
-
-    vps = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          ./machines/base.nix
-          ./machines/vps/vps.nix
-        ];
-      };
-
-    server = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          ./machines/base.nix
-          #./machines/server/vps.nix
         ];
       };
     };
@@ -66,18 +38,11 @@
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
-      "colin@desktop" = home-manager.lib.homeManagerConfiguration {
+      "colin@go" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         # > Our main home-manager configuration file <
-        modules = [./profiles/desktop.nix];
-      };
-
-        "colin@go" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-          extraSpecialArgs = {inherit inputs outputs;};
-          # > Our main home-manager configuration file <
-          modules = [./profiles/go.nix];
+        modules = [./profiles/go.nix];
       };
     };
   };
